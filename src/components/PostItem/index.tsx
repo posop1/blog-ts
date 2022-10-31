33b14eb95abe24602de50e9c90a8ea30b04301c4
Artur Yakhin <artur.yakhin52@gmail.com>
2022-10-31 10:56:56 +0500

    postItem

--- src/components/PostItem/index.tsx
+++ src/components/PostItem/index.tsx
@@ -3,7 +3,14 @@ import { IPost } from '../../types/types'
 import styles from './PostItem.module.scss'
 
 const PostItem: FC<IPost> = ({ id, title, body }) => {
-  return <div className={styles.PostItem}>item</div>
+  return (
+    <div className={styles.PostItem}>
+      <div style={{ display: 'flex' }}>
+        <p>{id}</p>.<h3>{title}</h3>
+      </div>
+      <p>{body}</p>
+    </div>
+  )
 }
 
 export default PostItem
